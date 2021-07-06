fdfd62baf5e8ae5f8ff0326379d73f36b657d974
ce424-992-97110422 <ce424-992-97110422@ce.tarasht.edu>
2021-07-06 15:42:16 +0430

    better naming

--- main.cu
+++ main.cu
@@ -7,7 +7,7 @@
 #define FROM_FILE 2
 
 #define BLOCK_DIM 1<<10
-#define COL_P_BLK 5
+#define COL_PER_BLK 5
 using namespace std;
 
 __device__ void normalize_row(const double *base_row, double *target_row, double scale, size_t n, size_t base_offset,
@@ -28,17 +28,17 @@ __global__ void gje_inverse(double *m2d, size_t n, size_t cr, double *scl) {
     extern __shared__ double mr[];
     unsigned int tid = threadIdx.x;
     unsigned int bid = blockIdx.x;
-    unsigned int ofs = COL_P_BLK * bid;
+    unsigned int ofs = COL_PER_BLK * bid;
 
     if (tid == 0) {
-        for (size_t i = 0; i < COL_P_BLK; ++i)
+        for (size_t i = 0; i < COL_PER_BLK; ++i)
             mr[i] = m2d[(cr * m2d_width) + (ofs + i)];
     }
     __syncthreads();
     if (tid == cr) {
-        normalize_self(&m2d[tid * m2d_width], scl[tid], COL_P_BLK, ofs);
+        normalize_self(&m2d[tid * m2d_width], scl[tid], COL_PER_BLK, ofs);
     } else
-        normalize_row(mr, &m2d[tid * m2d_width], scl[tid], COL_P_BLK, 0, ofs);
+        normalize_row(mr, &m2d[tid * m2d_width], scl[tid], COL_PER_BLK, 0, ofs);
 }
 
 __global__ void gje_scale_calc(double *m2d, size_t n, size_t current_row, double *scale) {
@@ -97,7 +97,7 @@ int main(int argc, char **argv) {
     print_matrix(m_h, n, n);
 
     size_t m2d_width = 2 * n;
-    double *m2d = nullptr, *scl_d = nullptr;
+    double *m2d = nullptr, *scale_d = nullptr;
     int error = 0;
     error |= cudaMalloc((void **) &m2d, n * m2d_width * sizeof(double));
     for (size_t i = 0; i < n; ++i) {
@@ -105,8 +105,8 @@ int main(int argc, char **argv) {
     }
 
     dim3 block_dim(BLOCK_DIM);
-    dim3 grid_dim((2 * n) / COL_P_BLK + ((2 * n) % COL_P_BLK != 0));
-    error |= cudaMalloc((void **) &scl_d, n * sizeof(double));
+    dim3 grid_dim((2 * n) / COL_PER_BLK + ((2 * n) % COL_PER_BLK != 0));
+    error |= cudaMalloc((void **) &scale_d, n * sizeof(double));
     if (error != cudaSuccess) {
         cout << "couldn't allocate memory in device";
         cout << cudaGetErrorString((cudaError_t) error);
@@ -123,14 +123,14 @@ int main(int argc, char **argv) {
 //int i=0;
     for (size_t i = 0; i < n; i++) {
 
-        gje_scale_calc<<<1, block_dim>>>(m2d, n, i, scl_d);
+        gje_scale_calc<<<1, block_dim>>>(m2d, n, i, scale_d);
         cudaDeviceSynchronize();
         double *temp = (double *) malloc(sizeof(double) * n);
-        error |= cudaMemcpy(temp, scl_d, sizeof(double) * n, cudaMemcpyDeviceToHost);
+        error |= cudaMemcpy(temp, scale_d, sizeof(double) * n, cudaMemcpyDeviceToHost);
 
         for (int i = 0; i < n; ++i)cout<<temp[i]<<"\t";cout<<"\n";
 
-        gje_inverse<<<grid_dim, block_dim, COL_P_BLK * sizeof(double)>>>(m2d, n, i, scl_d);
+        gje_inverse<<<grid_dim, block_dim, COL_PER_BLK * sizeof(double)>>>(m2d, n, i, scale_d);
         cudaDeviceSynchronize();
     }
     for (size_t i = 0; i < n; ++i) {
